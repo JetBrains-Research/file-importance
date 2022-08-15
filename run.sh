217fbec2fa58264abe80632db6207e6d2042e2b5
vharatian <vharatian@gmail.com>
2022-08-15 15:23:24 +0300

    Add parameters to python code

--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
-if [ $# -ne "3" ]; then
-  echo "usage: run <dependency level> <path to project> <path to output folder>"
+if [ $# -ne "2" ]; then
+  echo "usage: run <dependency level> <path to project>"
   exit 1
 fi
 
 projectPath="$(cd "$(dirname "$2")"; pwd)/$(basename "$2")"
-sharedFolder="$(cd "$(dirname "$3")"; pwd)/$(basename "$3")"
 
 graphMiner="DependencyGraph"
+graphAnalyzer="DependencyGraphAnalysis"
+outputFolderName="./output"
+mkdir "$outputFolderName"
 
-"./$graphMiner/gradlew" -p "./$graphMiner" extractDependencies -Pdeplevel="$1" -Pprojectpath="$projectPath" -Poutputpath="$sharedFolder"
+outputFolderPath="$(cd "$(dirname "$outputFolderName")"; pwd)/$(basename "$outputFolderName")"
+
+graphFilePath="$outputFolderPath/graph.json"
+infoFilePath="$outputFolderPath/info.json"
+graphImagePath="$outputFolderPath/graph.png"
+
+"./$graphMiner/gradlew" -p "./$graphMiner" extractDependencies -Pdeplevel="$1" -Pprojectpath="$projectPath" -Poutputpath="$outputFolderPath"
+
+
+cd "$graphAnalyzer"
+pip3 install -r requirements.txt
+python3 ./code/DependencyGraphEvaluator.py "$graphFilePath" "$graphImagePath"
