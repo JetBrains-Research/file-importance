b0f07536f41ece21bc8b8c7806b11f1a600b1957
vharatian <vharatian@gmail.com>
2023-01-16 13:01:48 +0300

    Finalized repository finder

--- DependencyGraphAnalysis/src/RepositoryFinder.py
+++ DependencyGraphAnalysis/src/RepositoryFinder.py
@@ -1,20 +1,108 @@
+import xlsxwriter
+
 from github import Github
 
 
-def print_repositories(repositories):
-    count = 0
+def process_repository(repositories):
     for repo in repositories:
         languages = repo.get_languages()
-        print(f"{repo.name} : {repo.stargazers_count} *** languages: {languages}")
-        count += 1
+        total_language = 0
+        jk_language = 0
+        for l in languages:
+            if l == 'Java' or l == 'Kotlin':
+                jk_language += languages[l]
 
-        if count > 5:
+            total_language += languages[l]
+
+        coverage = 0
+        if total_language > 0:
+            coverage = jk_language / total_language
+
+        repo.lang_coverage = coverage
+        repo.lang = languages
+
+
+def print_repositories(repositories):
+    for repo in repositories:
+        languages = repo.get_languages()
+        print(f"{repo.name} : {repo.stargazers_count} *** {repo.lang_coverage} -> {languages}")
+
+
+def export_results(repositories):
+    workbook = xlsxwriter.Workbook("test.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    centered_text = workbook.add_format({
+        'align': 'center',
+        'valign': 'vcenter',
+        'font_size': 10})
+
+    left_text_format = workbook.add_format({
+        'align': 'left',
+        'valign': 'vcenter',
+        'font_size': 10})
+
+    header_format = workbook.add_format({
+        'bold': 1,
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': '000080',
+        'font_color': 'white'})
+
+    worksheet.set_row(0, 40)
+    worksheet.set_column(0, 0, 50)
+    for i in range(1, 5):
+        worksheet.set_column(i, i, 25)
+
+    worksheet.write(0, 0, "Repository", header_format)
+    worksheet.write(0, 1, "Stars", header_format)
+    worksheet.write(0, 2, "Java Kotlin Percentage", header_format)
+    worksheet.write(0, 3, "Language Name", header_format)
+    worksheet.write(0, 4, "File Size", header_format)
+
+    row = 1
+    for repo in repositories:
+        first_row = row
+        for l in repo.lang:
+            worksheet.write(row, 3, l, centered_text)
+            worksheet.write(row, 4, repo.lang[l], centered_text)
+            row = row + 1
+
+        if row == first_row:
+            row = row + 1
+
+        if row - first_row > 1:
+            worksheet.merge_range(first_row, 0, row-1, 0, repo.name, left_text_format)
+            worksheet.merge_range(first_row, 1, row-1, 1, repo.stargazers_count, centered_text)
+            worksheet.merge_range(first_row, 2, row-1, 2, "%.2f" % repo.lang_coverage, centered_text)
+        else:
+            worksheet.write(first_row, 0, repo.name, left_text_format)
+            worksheet.write(first_row, 1, repo.stargazers_count, centered_text)
+            worksheet.write(first_row, 2, "%.2f" % repo.lang_coverage, centered_text)
+
+    workbook.close()
+
+
+def get_first(it, count):
+    result = []
+    for i in it:
+        result += [i]
+        if len(result) >= count:
             break
 
+    return result
 
-g = Github()
 
-repositories = g.search_repositories(query='Java', sort='stars', order='desc')
-print_repositories(repositories)
-repositories = g.search_repositories(query='Kotlin', sort='stars', order='desc')
-print_repositories(repositories)
+g = Github(login_or_token="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")
+queries = ["Java", "Kotlin"]
+all_repositories = []
+for q in queries:
+    fetched_repositories = g.search_repositories(query=q, sort='stars', order='desc')
+    repositories = get_first(fetched_repositories, 100)
+    process_repository(repositories)
+    all_repositories += repositories
+
+
+sorted(all_repositories, key=lambda r: r.stargazers_count, reverse=True)
+print_repositories(all_repositories)
+export_results(all_repositories)
