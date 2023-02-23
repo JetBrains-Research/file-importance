b2b55e177acfc081ca298760de4892951a64aa5d
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2023-02-23 12:09:26 +0100

    remove unused code

--- DependencyGraph/src/main/kotlin/org/jetbrains/research/ictl/fileimportance/Utils.kt
+++ DependencyGraph/src/main/kotlin/org/jetbrains/research/ictl/fileimportance/Utils.kt
@@ -1,9 +1,5 @@
 package org.jetbrains.research.ictl.fileimportance
 
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiManager
-import com.intellij.psi.search.FilenameIndex
-import com.intellij.psi.search.GlobalSearchScope
 import kotlin.system.exitProcess
 
 object Utils {
@@ -18,19 +14,6 @@ object Utils {
 
 inline fun <reified T> log(log: T) {
     println("****Miner**** $log")
-}
-
-fun getPsiFiles(project: Project, ext: String) = sequence {
-    log("get all $ext files")
-    val psiManager = PsiManager.getInstance(project)
-
-    FilenameIndex.getAllFilesByExt(project, ext, GlobalSearchScope.projectScope(project)).forEach {
-        val file = psiManager.findFile(it)
-        // yieldNotNull raises java.lang.ClassNotFoundException: org.jetbrains.kotlin.utils.CollectionsKt
-        file?.let {
-            yield(file)
-        }
-    }
 }
 
 /**
